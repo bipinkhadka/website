368abf450af6a6d7ec98a7c386269dfefd36d3c8
Bipin khadka <khadkabipin10@yahoo.com>
2024-03-18 17:34:41 +1100

    javascriptcommit

---
+++ script.js
@@ -0,0 +1,11 @@
+//javascript code 
+
+class random{
+  constructor(constructerforwebsite , anotherconstructer ){
+    this.constructerforwebsite = reviousconstructer
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+   }
+  
+  clear()
+}
